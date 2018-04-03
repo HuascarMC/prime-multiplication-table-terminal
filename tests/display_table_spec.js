cc12b1429c95db35923b3844f132568a9ef6cbb6
Huascar <huascarmc@hotmail.com>
2018-04-03 18:47:08 +0100

    added display table spec

---
+++ tests/display_table_spec.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var DisplayTable = require('../models/display_table');
+
+describe('DisplayTable', function () {
+
+ var displayTable;
+
+ beforeEach(function () {
+  displayTable = new DisplayTable();
+ });
+
+});
