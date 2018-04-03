b9702e024164b5a69a154bca4f2a352c010e3009
Huascar <huascarmc@hotmail.com>
2018-04-03 17:07:08 +0100

    set up test file for primable class

---
+++ tests/primable_spec.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var Primable = require('../models/primable');
+
+escribe('Primable', function () {
+
+ var primable;
+
+ beforeEach(function () {
+  primable = new Primable();
+ });
+
+
+});
