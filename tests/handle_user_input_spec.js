618a4920d5075b0afa9833e5bb20fbe980fb2436
Huascar <huascarmc@hotmail.com>
2018-04-03 19:25:27 +0100

    added test should ask user for number

--- tests/handle_user_input_spec.js
+++ tests/handle_user_input_spec.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var HandleUserInput = require('../models/handle_user_input.js');
+var robot = require('robotjs')
 
 describe('HandleUserInput', function () {
 
@@ -11,6 +12,8 @@ describe('HandleUserInput', function () {
 
  it('should be able to ask user for number on terminal' function () {
   const result = handleUserInput.getNumber();
+  robot.typeString('7');
+  robot.keyTap('enter');
   assert.strictEqual(result, '7');
  });
 });
