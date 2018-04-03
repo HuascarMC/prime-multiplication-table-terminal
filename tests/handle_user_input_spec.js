d88c7cfd8ffb1b4caa9760c9d18fe3433986af40
Huascar <huascarmc@hotmail.com>
2018-04-03 19:05:44 +0100

    added handle user input spec

---
+++ tests/handle_user_input_spec.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var HandleUserInput = require('../models/handle_user_input.js');
+
+describe('HandleUserInput', function () {
+
+ var handleUserInput;
+
+ beforeEach(function () {
+  handleUserInput = new HandleUserInput();
+ });
+
+
+});
