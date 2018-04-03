474d67ab285a014415c9fa2677e99690460ffcfc
Huascar <huascarmc@hotmail.com>
2018-04-03 19:00:18 +0100

    added test display table logs table

--- tests/display_table_spec.js
+++ tests/display_table_spec.js
@@ -9,4 +9,11 @@ describe('DisplayTable', function () {
   displayTable = new DisplayTable();
  });
 
+ it('should console log the table', function () {
+  const actualResult = displayTable.log([[1,2],
+                                        [2,4]]);
+  const expectedResult = [[1,2],[2,4]];
+  assert.deepEqual(actualResult, expectedResult);
+ });
+
 });
