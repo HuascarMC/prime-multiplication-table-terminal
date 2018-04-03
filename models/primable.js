48320fbe20a557639d0215cbd842f7cd8f61629c
Huascar <huascarmc@hotmail.com>
2018-04-03 17:14:48 +0100

    checks if number is/not prime tests passed

--- models/primable.js
+++ models/primable.js
@@ -1,6 +1,12 @@
-var Primable = function () {
+var Primable = function () {}
 
+Primable.prototype.isPrime = function(number) {
+ if(number % 2 === 0 && number !== 2) {
+  return false
+ }
+ return true
 }
+
 
 
 module.exports = Primable;
