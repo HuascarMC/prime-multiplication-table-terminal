3c753aa6b04074b1d2ec59f3e6947191711b95ea
Huascar <huascarmc@hotmail.com>
2018-04-03 17:12:39 +0100

    it should include self number if prime getPrimeNumbers

--- tests/primable_spec.js
+++ tests/primable_spec.js
@@ -15,14 +15,18 @@ escribe('Primable', function () {
 
  it('should check if a number is not prime', function () {
  assert.strictEqual(primable.isPrime(4), false)
-});
+ });
 
  it('should get all prime numbers from zero to a given number', function () {
   const number = 10;
   result = [2, 3, 5, 7, 9]
   assert.deepEqual(primable.getPrimes(number), result);
-});
+ });
 
-
+it('should get all prime numbers from zero to a given number and INCLUDE that number if prime', function() {
+  const number = 15;
+  result = [2, 3, 5, 7, 9, 11, 13, 15];
+  assert.deepEqual(primable.getPrimes(number), result)
+ })
 
 });
