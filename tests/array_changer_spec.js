40bc8c0624332af7e35efbd137a675b157350d18
Huascar <huascarmc@hotmail.com>
2018-04-03 17:35:05 +0100

    created array changer spec file

---
+++ tests/array_changer_spec.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var ArrayChanger = require('../models/array_changer');
+
+describe('array_changer', function () {
+
+ var arrayChanger;
+
+ beforeEach(function () {
+  arrayChanger = new ArrayChanger();
+ });
+
+});
