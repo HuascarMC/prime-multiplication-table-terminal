c5ecd3a740a1a84c57a5741bf4d95837e93a1b90
Huascar <huascarmc@hotmail.com>
2018-04-05 18:15:19 +0100

    updated user prompt

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var rl = readline.createInterface(process.stdin, process.stdout);
 var tableCreater = new TableCreater();
 var displayTable = new DisplayTable();
 
-rl.question("Choose a number to generate a prime multiplication table: ", function(answer){
+rl.question("Choose a number between 1-67 to generate a prime multiplication table: ", function(answer){
  if(isNaN(answer) == false) {
   console.log("Thank you.");
   console.log(parseInt(answer))
